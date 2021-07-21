e9531c2ade01a44cc772c80d61131029175e7f8b
Odeseos <bjornhage1998@gmail.com>
2021-07-21 12:18:36 +0200

    Compacted the code, added working pagination with page list length and hideable columns

--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import datetime
 import webbrowser
 import sqlite3
 import db_actions
@@ -27,26 +28,22 @@ def add_entry_form():
     if request.method == 'GET':
         return render_template('add_entry.html')
 
-@app.route('/this_month')
-def this_month():
+@app.route('/get_month/<month>')
+def get_month(month):
     with sqlite3.connect('HTF_Hour_Database.db') as conn:
         c = conn.cursor()
-    data = db_actions.get_data_for_this_month(c, request.args.get('m'))
+    if month == "all":
+        data = db_actions.get_all_data(c)
+        month_name = "everything"
+        title = "All Entries"
+    elif int(month) >= 1 and int(month) <= 12:
+        data = db_actions.get_data_for_this_month(c, month)
+        month_name = datetime.datetime.strptime(month, '%m').strftime('%B')
+        title = "Entries from " + month_name
     hours_worked = 0
     for rows in data:
         hours_worked += rows[4]
-    return render_template('this_month.html', data=[data, hours_worked])
-
-
-@app.route('/all_entries')
-def all_entries():
-    with sqlite3.connect('HTF_Hour_Database.db') as conn:
-        c = conn.cursor()
-    data = db_actions.get_all_data(c)
-    hours_worked = 0
-    for rows in data:
-        hours_worked += rows[4]
-    return render_template('all_entries.html', data=[data, hours_worked])
+    return render_template('get_month.html', data=[data, hours_worked, month_name, title])
 
 # if __name__ == '__main__':
 #     app.run()
